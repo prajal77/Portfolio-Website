55e4ff7257d394a2887ad07833a34dd519aab28a
prajal77 <61044182+prajal77@users.noreply.github.com>
2023-04-20 09:22:04 +0545

    commit

--- pages/home.js
+++ pages/home.js
@@ -4,9 +4,16 @@ const Home = () => {
     return <>
         <NavBar />
         <section className="flex w-full text-white bg-black">
-            <div className="w-full bg-red-950">
-                <h1>Hello! I'm  Prajwol Basnet</h1>
-                <p>Frontend Developer</p>
+            <div className="flex flex-row items-center w-full bg-red-950">
+                <div className="">
+                    <h1 className="text-xl">Hello!</h1>
+                    <br />
+                    <h1 className="text-6xl font-bold"> I'm  Prajwol Basnet</h1>
+                </div>
+                <br />
+                <div className="inline-block">
+                    <p>Frontend Developer</p>
+                </div>
             </div>
             <div className="w-full bg-red-500">
                 <img src="/photo.png" />
